318385675ad0fb2c19c2c03f52b157f885b36f0b
GokulVelavan <gokulvelavans@gmail.com>
2021-10-10 21:37:19 +0530

    uc-2

--- Program.cs
+++ Program.cs
@@ -8,9 +8,18 @@ namespace SnackandLadder
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Snack AND Ladder");
+            int POSITION = 0;
+            int PLAYER = 1;
+            int DICE = 0;
+            int COUNT = 0;
+            Random random = new Random();
+            DICE = random.Next(0, 7);
+            Console.WriteLine("THE STARTING POSITION IS:{0}", COUNT);
+            Console.WriteLine("THE STARTING DICE NO IS:{0}", DICE);
+
             Console.ReadLine();
         }
     }
