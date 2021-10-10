d651498896bb96dffd726ec95078dce4979afbdc
GokulVelavan <gokulvelavans@gmail.com>
2021-10-10 21:42:36 +0530

    uc-2

--- Program.cs
+++ Program.cs
@@ -15,7 +15,8 @@ namespace SnackandLadder
             int PLAYER = 1;
             int DICE = 0;
             int COUNT = 0;
-            Random random = new Random();
+            Random random = new Random();   
+
             DICE = random.Next(0, 7);
             Console.WriteLine("THE STARTING POSITION IS:{0}", COUNT);
             Console.WriteLine("THE STARTING DICE NO IS:{0}", DICE);
